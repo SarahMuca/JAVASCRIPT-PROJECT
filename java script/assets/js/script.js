8b8ddf0fa97fcf5c3f851aa8ca6e6b830d7356c1
SarahMuca <115831619+SarahMuca@users.noreply.github.com>
2023-08-15 14:16:00 +0200

    js project
    js project

---
+++ java script/assets/js/script.js
@@ -0,0 +1,169 @@
+ //navbari
+ 'use strict';
+ const overlay = document.querySelector("[data-overlay]");
+ const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [overlay, navOpenBtn, navCloseBtn];
+for (let i = 0; i < navElems.length; i++) {
+    navElems[i].addEventListener("click", function () {
+     navbar.classList.toggle("active");
+     overlay.classList.toggle("active");
+    });
+}
+
+
+
+//butoni go to top
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 80) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+//popupi i reklames
+
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+
+window.onload = function(){
+    setTimeout(function(){
+        popup.style.display = "block";
+    }, 4000)
+}
+
+close.addEventListener('click', () => {
+     popup.style.display = "none";
+})
+
+
+function openPopup()
+{
+  window.open('file:///C:/Users/Dell/OneDrive/Desktop/kosova/java%20script/carrito-de-compras-main%20(1)/carrito-de-compras-main/prod.html','width=800, height=500');
+}
+
+
+function openProdp()
+{
+  window.open('file:///C:/Users/Dell/OneDrive/Desktop/kosova/java%20script/carrito-de-compras-main%20(1)/carrito-de-compras-main/prod.html');
+}
+
+
+
+
+
+
+
+
+const textarea = document.querySelector('.chatbox-message-input')
+const chatboxForm = document.querySelector('.chatbox-message-form')
+
+textarea.addEventListener('input', function () {
+	let line = textarea.value.split('\n').length
+
+	if(textarea.rows < 6 || line < 6) {
+		textarea.rows = line
+	}
+
+	if(textarea.rows > 1) {
+		chatboxForm.style.alignItems = 'flex-end'
+	} else {
+		chatboxForm.style.alignItems = 'center'
+	}
+})
+
+
+
+const chatboxToggle = document.querySelector('.chatbox-toggle')
+const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
+
+chatboxToggle.addEventListener('click', function () {
+	chatboxMessage.classList.toggle('show')
+})
+
+
+
+const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle')
+const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu')
+
+dropdownToggle.addEventListener('click', function () {
+	dropdownMenu.classList.toggle('show')
+})
+
+document.addEventListener('click', function (e) {
+	if(!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
+		dropdownMenu.classList.remove('show')
+	}
+})
+
+
+// mezashi qe dergojm
+const chatboxMessageWrapper = document.querySelector('.chatbox-message-content')
+const chatboxNoMessage = document.querySelector('.chatbox-message-no-message')
+
+chatboxForm.addEventListener('submit', function (e) {
+	e.preventDefault()
+
+	if(isValid(textarea.value)) {
+		writeMessage()
+		setTimeout(autoReply, 1000)
+	}
+})
+
+
+
+function addZero(num) {
+	return num < 10 ? '0'+num : num
+}
+
+function writeMessage() {
+	const today = new Date()
+	let message = `
+		<div class="chatbox-message-item sent">
+			<span class="chatbox-message-item-text">
+				${textarea.value.trim().replace(/\n/g, '<br>\n')}
+			</span>
+			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+		</div>
+	`
+	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
+	chatboxForm.style.alignItems = 'center'
+	textarea.rows = 1
+	textarea.focus()
+	textarea.value = ''
+	chatboxNoMessage.style.display = 'none'
+	scrollBottom()
+}
+
+function autoReply() {
+	const today = new Date()
+	let message = `
+		<div class="chatbox-message-item received">
+			<span class="chatbox-message-item-text">
+				Thank you for your awesome support!
+			</span>
+			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+		</div>
+	`
+	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
+	scrollBottom()
+}
+
+function scrollBottom() {
+	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
+}
+
+function isValid(value) {
+	let text = value.replace(/\n/g, '')
+	text = text.replace(/\s/g, '')
+
+	return text.length > 0
+}
---
+++ java script/first.js
@@ -0,0 +1,19 @@
+function Redirect()
+ {
+    window.location = "c:\Users\Dell\OneDrive\Desktop\kosova\java script\index.html"
+ }
+
+
+
+function toggle() {
+   var x = document.getElementById("div1");
+    if(x.style.display === "none") {
+      x.style.display = "block";
+    }
+
+    else {
+      x.style.display = "none";
+    }
+}
+
+
